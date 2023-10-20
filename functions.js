71159254dc9b2634b31a773081887c8aae9899b0
RedFalconXQ <javierequijanof@gmail.com>
2023-10-19 22:01:59 -0500

    Creating the webpage for Grade Report (index.html) and adding parameters (functions.js)

---
+++ functions.js
@@ -0,0 +1,13 @@
+/*
+Develope a Student Grade Report web page
+
+1. Create an array with five grades
+2. Display 4 buttons
+    2.1. Display list with all grades
+    2.2. Calculate average
+    2.3. Get the highest grade
+    2.4. Is a failing grade found?
+
+NOTE: Validate the grades' values from 0.0 to 5.0
+      If there is a failing grade, the grade must be below 3.0 (insufficient)
+*/
